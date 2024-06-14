dad02e3bba8f7700b5ed436bad4452bacc5accb4
Kapparov Murat <paprim2003@gmail.com>
2024-06-14 21:35:44 +0500

    kafka

--- KafkaStudyProj/src/main/java/com/example/kafkastudyproj/KafkaStudyProjApplication.java
+++ KafkaStudyProj/src/main/java/com/example/kafkastudyproj/KafkaStudyProjApplication.java
@@ -1,7 +1,11 @@
 package com.example.kafkastudyproj;
 
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class KafkaStudyProjApplication {
@@ -9,5 +13,10 @@ public class KafkaStudyProjApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaStudyProjApplication.class, args);
 	}
-
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String , String> kafkaTemplate){
+		return args -> {
+			for (int i = 0; i < 1000; i++) kafkaTemplate.send("kafkastudy","hello kafka");
+		};
+	}
 }
